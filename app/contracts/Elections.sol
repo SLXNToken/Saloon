3dfa028c1cf9f39af4e87cd106ad38a44bf3776e
Joshua Caputo <jos2244923@yahoo.com>
2018-08-16 21:03:22 -0700

    yawn

--- app/contracts/Elections.sol
+++ app/contracts/Elections.sol
@@ -2,38 +2,69 @@ pragma solidity ^0.4.24;
 
 contract Elections {
 
-	// Voter Model
-	struct Voter {
-		bool voted;
-	}
-	// Voter Mapping
-	mapping(address => bool) public voters;
+	/*/ Initialize Contract */
+	constructor() public {
+		addMode('1v1', 1, 100, 2);
+		addMode('2v2', 1, 100, 4);
+  	}
 
-	// Candidate Model
-	struct Candidate {
+	/* Modes */
+	struct Mode {
 		uint id;
 		string name;
-		uint voteCount;
+		uint minBet;
+		uint maxBet;
+		uint usersNeeded;
 	}
-	// Candidate Mapping (Fetch / Store)
-	mapping(uint => Candidate) public candidates;
-	// Candidate Count
-	uint public candidateCount;
+	mapping(uint => Mode) public modes; uint public modeCount;
 
-	constructor() public {
-		addCandidate('Player #1');
-		addCandidate('Player #5');
+  	function addMode (string _name, uint _minBet, uint _maxBet, uint _maxPlayers) private {
+  		modeCount ++;
+  		modes[modeCount] = Mode(modeCount, _name, _minBet, _maxBet,_maxPlayers);
   	}
 
-  	function addCandidate (string _name) private {
-  		candidateCount ++;
-  		candidates[candidateCount] = Candidate(candidateCount, _name, 0);
+  	/* Matches */
+  	struct Match {
+  		uint id;
+  		uint mode;
+  		uint bet;
+  		address founder;
+  		mapping(address => address) vote;
+  	}
+	mapping(uint => Match) public matches; uint public matchCount;
+  	function addMatch (uint _mode, uint _bet, address _founder) private {
+
+  		// Add the match to the mapping
+  		matchCount ++;
+  		matches[matchCount] = Match(matchCount, _mode, _bet, _founder);
+
+  		// Place the user in the Match
+  		userInGame[_founder] = true;
+  		usersGame[_founder] = matchCount;
+  	}
+  	function addVote (address _for) private{
+  		// Add the vote to the vote mapping of the match
+  		matches[usersGame[msg.sender]].vote[msg.sender] = _for;
   	}
 
-  	function vote (uint _candidateId) public {
-  		require(!voters[msg.sender]);
-  		require(_candidateId > 0 && _candidateId <= candidateCount);
-  		voters[msg.sender] = true;
-  		candidates[_candidateId].voteCount ++;
+  	/* Users -> Matches */
+	mapping(address => bool) public userInGame;
+	mapping(address => uint) public usersGame;
+
+  	function createMatch (uint _mode, uint _bet) public {
+  		require(modes[_mode].id != 0); // Mode must exist
+  		require(_bet >= modes[_mode].minBet && _bet <= modes[_mode].maxBet); // Bet must be between minBet and maxBet
+  		require(!userInGame[msg.sender]); // The user can not already be in a game
+
+  		// Create the Match
+  		addMatch(_mode, _bet, msg.sender);
+  	}
+  	function castMatchVote (address _for) public {
+  		require(userInGame[msg.sender]); // User must be in a match
+  		require(matches[usersGame[msg.sender]].id!=0); // The match the user is in must exist
+  		require(matches[usersGame[msg.sender]].vote[msg.sender]==0); // The user must not have already vote in this match
+
+  		// Add the vote
+  		addVote(_for);
   	}
 }
