93d4eb73bab29d45a9986c5d4c1e59f0ed0f086e
Joshua Caputo <jos2244923@yahoo.com>
2018-08-24 22:15:18 -0700

    first agreement tests

--- app/contracts/Saloon.sol
+++ app/contracts/Saloon.sol
@@ -28,24 +28,40 @@ contract Saloon {
   	uint id;
   	uint mode;
   	uint stake;
-  	address founder;
-  	mapping(address => address) vote;
-  	mapping(uint => address) accounts; uint numAccounts;
-    mapping(address => string) epic;
+    uint numAccounts;
+    mapping(uint => address) accounts;
+      mapping(address => uint) voteCount;
+      mapping(address => bool) hasVoted;
+      mapping(address => string) epic;
   }
 	mapping(uint => Match) public matches; uint public matchCount;
+
   event addedMatchEvent ( uint indexed matchId );
 	function addMatch (uint _mode, uint _stake, address _founder, string _epic) private {
 
 		// Add the match to the mapping
 		matchCount ++;
-		matches[matchCount] = Match(matchCount, _mode, _stake, _founder, 0);
+		matches[matchCount] = Match(matchCount, _mode, _stake, 0);
 
 		// Place the user in the Match's account mapping
 		placeUserInMatch(matchCount, _founder, _epic);
+
     // Trigger event
-    addedMatchEvent(matchCount);
+    emit addedMatchEvent(matchCount);
 	}
+  function getAccountByIDinMatch(uint _matchID, uint account) public constant returns(address votes)
+  {
+     return matches[_matchID].accounts[account];
+  }  
+  function getVotesForAccountInMatch(uint _matchID, address account) public constant returns(uint votes)
+  {
+     return matches[_matchID].voteCount[account];
+  }  
+  function getHasVotedForAccountInMatch(uint _matchID, address account) public constant returns(bool voted)
+  {
+     return matches[_matchID].hasVoted[account];
+  }
+
 	function placeUserInMatch (uint _id, address _address, string _epic) private {
 
 		// Place the user in the Match !merge-!refactor-these
@@ -53,81 +69,89 @@ contract Saloon {
 		usersGame[_address] = matchCount;
     // Place the user in the Match !merge-!refactor-these
     usersEpic[_address] = _epic;
+
 		// Place the user in the Match's account mapping
 		matches[_id].numAccounts ++;
 		matches[_id].accounts[matches[_id].numAccounts] = _address;
-
 	}
-	function addVote (address _for) private {
-		// Add the vote to the vote mapping of the match
-		matches[usersGame[msg.sender]].vote[msg.sender] = _for;
 
-    if (everyoneVoted(matches[usersGame[msg.sender]].id) == true){
-
+  function endMatch (uint _id, address _winner) private {
+    usersWins[_winner] ++;
+    for (uint i = 0; i < matches[_id].numAccounts; i++){ // loop through accounts in match
+      delete userInGame[matches[_id].accounts[i+1]];
+      delete usersGame[matches[_id].accounts[i+1]];
     }
-	}
-	function everyoneVoted (uint _id) private view returns (bool _allVotesAreIn){
-		for (uint i = 0; i < matches[_id].numAccounts; i++){ // all connected accounts must have voted for someone
-			if (matches[_id].vote[matches[_id].accounts[i]]==0) return false; // someone hasn't voted yet
-		}
-    return true; // noone hasn't voted
-	}
-  function evaluateThread (uint _id) private {
-
-  // CREATE VARIABLES
-    // for all accounts in a thread mapping(account => uint) votes;
-      // get vote for each account
-      // require vote is for account in thread mapping, else, don't count it 
-      // increment votes[vote] by one
-
-  // PUT VARIABLES UNDER CONDITIONS
-    // create an empty winner address winner;
-    // for all accounts in a thread mapping's votes
-      // if (account's votes > 0) 
-        // if winner is unset 
-          // winner = account;
-        // else 
-          // if (account's votes > 0) dispute = true;
-      //
-
-  // MANAGE OUTCOME
-    // if (dispute is true)
-      // createDispute();
-    // else
-      // awardWinner(winner);
 
   }
 
-  	/* Users -> Matches */
-	mapping(address => bool) public userInGame;
-	mapping(address => uint) public usersGame;
+  /* Users -> Matches */
   mapping(address => string) private usersEpic;
+
+
+
+  mapping(address => bool) public userInGame;
+  mapping(address => uint) private usersGame;
+  mapping(address => uint) public usersWins;
 
   function createMatch (uint _mode, uint _stake, string _epic) public {
   	// User must have stake
+    require(!userInGame[msg.sender]); // user can not already be in a game
   	require(modes[_mode].id != 0, "_mode must exist");
   	require(_stake >= modes[_mode].minStake && _stake <= modes[_mode].maxStake); // stake must be stakeween minStake and maxStake
-  	require(!userInGame[msg.sender]); // user can not already be in a game
 
-  	// Create the Match
   	addMatch(_mode, _stake, msg.sender, _epic);
   }
   function joinMatch (uint _id, string _epic) public{
   	// User must have stake
  	  require(!userInGame[msg.sender]); // user can not already be in a match
  		require(matches[_id].id!=0); // match must exist
-  	require(modes[matches[_id].mode].usersNeeded > matches[_id].numAccounts); // Match must not be full match
+  	require(modes[matches[_id].mode].usersNeeded > matches[_id].numAccounts); // must not be full lobby
 
-  	placeUserInMatch(_id, msg.sender, _epic); // Place the user in the Match's account mapping
+  	placeUserInMatch(_id, msg.sender, _epic);
   }
-  	function castMatchVote (address _for) public {
-  		require(userInGame[msg.sender]); // User must be in a match
-  		require(matches[usersGame[msg.sender]].id!=0); // match the user is in must exist
-      require(_for!=0); // _for cannot be blank
-  		require(matches[usersGame[msg.sender]].vote[msg.sender] == 0); // user must not have already vote in this match
-  		require(modes[matches[usersGame[msg.sender]].mode].usersNeeded == matches[usersGame[msg.sender]].numAccounts); // Match must be full match.mode.usersNeeded == match.numAccounts
+  function castMatchVote (uint accountID) public{
+    address sender = msg.sender;
+    uint matchID = usersGame[sender];
+    address voteFor = matches[matchID].accounts[accountID];
 
-  		// Add the vote
-  		addVote(_for);
-  	}
+    require(userInGame[sender]); // user must be in a match
+    require(modes[matches[matchID].mode].usersNeeded == matches[matchID].numAccounts); // must be full lobby
+    require(matches[matchID].hasVoted[sender] == false); // user must not have voted
+    require(accountID <= matches[matchID].numAccounts && accountID > 0); // vote must be for player in match
+
+    matches[matchID].voteCount[voteFor] ++;
+    matches[matchID].hasVoted[sender] = true;
+
+    // check for flags
+    bool flag;
+    for (uint i = 0; i < matches[matchID].numAccounts; i++){ // loop through accounts in match
+      if (!matches[matchID].hasVoted[getAccountByIDinMatch(1, i+1)]){ // if any account hasn't voted
+        flag = true; // raise flag
+      }
+    }
+    // handle flags
+    if (flag == false){
+      address undisputedWinner;
+      bool disputed;
+      for (uint j = 0; j < matches[matchID].numAccounts; j++){ // loop through accounts in match
+        address thisAccount = matches[matchID].accounts[j+1];
+        uint voteCount = matches[matchID].voteCount[thisAccount];
+        if (voteCount > 0){
+          if (undisputedWinner == 0){
+            undisputedWinner = thisAccount;
+          }
+          else if (undisputedWinner != thisAccount){
+            disputed = true;
+          }
+        }
+      }
+      if (!disputed){
+        endMatch(matchID, undisputedWinner);
+      }
+      else {
+        // handle dispute contract
+      }
+    }
+  }
+
 }
